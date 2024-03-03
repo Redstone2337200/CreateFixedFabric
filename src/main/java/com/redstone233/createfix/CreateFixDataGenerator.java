5cca64e95133493ea957727ace87115f3138ef9f
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-03-03 15:17:34 +0800

    Add files via upload

---
+++ src/main/java/com/redstone233/createfix/CreateFixDataGenerator.java
@@ -0,0 +1,11 @@
+package com.redstone233.createfix;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreateFixDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
