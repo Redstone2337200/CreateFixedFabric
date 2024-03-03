31bde54a568d1a34fbc5aa2784103aed378d5194
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-03-03 13:44:18 +0800

    Update RenderTargetMixin.java

--- src/main/java/com/redstone233/createfix/mixin/RenderTargetMixin.java
+++ src/main/java/com/redstone233/createfix/mixin/RenderTargetMixin.java
@@ -1,1 +1,27 @@
 //test
+package com.redstone233.createfix.mixin;
+
+//import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/*
+@Mixin(MinecraftServer.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "loadWorld")
+	private void init(CallbackInfo info) {
+		// This code is injected into the start of MinecraftServer.loadWorld()V
+	}
+}
+*/
+
+@Mixin(RenderTargetMixin.class)
+public class RenderTargetMixin {
+  @Inject(at = @At("HEAD"), method = "loadWorld")
+   private void enableStencil() {
+      //CreateFix.LOGGER.info("method enableStencil has been patched");
+   }
+}
+
